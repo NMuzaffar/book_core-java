0518270a6b58b2b3b9d8b0e1bdc996ff7a891651
Muzaffar <muzaffar.nurmukhammadov@cybercon.de>
2022-02-17 00:10:12 +0100

    chapters 1, 2, 3

---
+++ src/main/java/fundamentals/FirstSample.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+/**
+ * This is the first sample program.
+ */
+public class FirstSample {
+
+  public static void main(String[] args) {
+    System.out.println("We will not use 'Hello, World!'");
+  }
+
+}
+ 
